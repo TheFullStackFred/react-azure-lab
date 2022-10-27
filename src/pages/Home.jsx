b4466f5f8c2e0284b228190027f5ed1f24d1675d
Fredrik Strömberg <fredrikstromberg90@gmail.com>
2022-10-27 23:08:59 +0200

    added fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import FetchData from '../components/FetchData'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <h1>Home</h1>
+      <FetchData />
+    </div>
+  )
 }
 
 export default Home

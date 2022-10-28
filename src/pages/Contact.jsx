cd08e2327b640ab5a8279be6df2b34054d09342a
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-10-28 19:18:08 +0200

    added contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
 const Contact = () => {
-  return <div>Contact</div>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <div id='contact'>
+      <form onSubmit={handleSubmit}>
+        <label>Name</label>
+        <input type='text' name='name' />
+        <label>Email</label>
+        <input type='email' name='email' />
+        <label>Message</label>
+        <textarea name='message' />
+        <input id='submit' type='submit' value='Send' />
+      </form>
+    </div>
+  )
 }
 
 export default Contact

7f34754136d63c6453a1404408ca047536b14b9d
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-10-28 18:18:03 +0200

    added about section and started to make it responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div id='about'>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ad
+        quas est tempore consectetur sequi ex, autem quibusdam ratione eaque
+        deserunt minus iusto quisquam, eveniet facere cupiditate provident
+        voluptatibus debitis consequuntur. Nobis corporis quis neque rem cumque
+        ipsam beatae deleniti reiciendis odio consequatur ut excepturi,
+        voluptate, aperiam numquam adipisci! Vitae.
+      </p>
+    </div>
+  )
 }
 
 export default About
